30449941062b60fa5711c4eeef23abb031640013
tunnelWithAC <conalldalydev@gmail.com>
2024-12-01 12:57:17 +0000

    tidy up

--- src/golang-v2/main.go
+++ src/golang-v2/main.go
@@ -17,14 +17,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s", name)
-}
-
 type Timestamp timestamppb.Timestamp
 
 func (t *Timestamp) UnmarshalJSON(data []byte) error {
@@ -55,7 +47,7 @@ func validate(v *protovalidate.Validator, msg protoreflect.ProtoMessage) error {
 	return nil
 }
 
-func validateHandler(w http.ResponseWriter, r *http.Request) {
+func nameHandler(w http.ResponseWriter, r *http.Request) {
 	v, err := protovalidate.New()
 	if err != nil {
 		panic(err)
@@ -65,16 +57,6 @@ func validateHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Name: ", name)
 
 	var errorList []error
-
-	if err = validate(v, &transaction.Transaction{
-		Id:           1000,
-		Price:        "$100.00",
-		PurchaseDate: timestamppb.New(time.Now().Add(-24 * time.Hour)), // 24 hours ago
-		DeliveryDate: timestamppb.New(time.Now()),
-		Email:        "bob@doe.mail",
-	}); err != nil {
-		errorList = append(errorList, err)
-	}
 
 	if err = validate(v, &hellov1.Name{
 		Name: name,
@@ -133,9 +115,9 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	http.HandleFunc("/validate", validateHandler)
-	fmt.Println("Server is running on port 8080...")
+	http.HandleFunc("/validate", nameHandler)
 	r.Post("/transactions", transactionHandler)
+	fmt.Println("Server is running on port 8080...")
 
 	http.ListenAndServe(":8080", r)
 }
