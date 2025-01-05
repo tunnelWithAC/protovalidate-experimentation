6775cc131fb204d049da769e91fd076cd8e17215
tunnelWithAC <conalldalydev@gmail.com>
2025-01-05 20:59:55 +0000

    add test project

---
+++ src/dotnet/DotProtoValidate.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DotProtoValidate.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
