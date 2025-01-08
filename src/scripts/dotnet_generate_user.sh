2cca8a2a8fdee4b94b7f550cab19acf0ce2fff21
tunnelWithAC <conalldalydev@gmail.com>
2025-01-08 21:29:44 +0000

    add proto file ref to csproj

--- src/scripts/dotnet_generate_user.sh
+++ src/scripts/dotnet_generate_user.sh
@@ -1,18 +1,4 @@
 # copy proto from main folder
-cp -r src/proto/* src/golang/lightweight-example/proto/
-
-# switch to go directory
-cd src/golang/lightweight-example
-
-# generate artefacts
-buf generate
-
-echo 'Generated Go files'
-ls gen
-
-# src/dotnet/ConsoleValidator/proto
-
-# copy proto from main folder
 cp -r src/proto/* src/dotnet/ConsoleValidator/proto/
 
 # switch to go directory
@@ -23,7 +9,7 @@ protoc \
   -I ./protovalidate/proto/protovalidate \
   --csharp_out=../Models \
   --proto_path=. \
-  ./*.proto
+  ./user.proto
 
 #protoc -I=./proto --python_out=./gen --grpc_python_out=./gen ./proto/*.proto
 
